d5300de5dfdb7b097efa4d8fd54f117830b6272f
Jared Masters <mastersjared3@gmail.com>
2025-11-24 14:02:25 -0800

    cleanup

--- src/LibraryInventory/Shelf.h
+++ src/LibraryInventory/Shelf.h
@@ -6,15 +6,8 @@
 
 class Shelf {
         public:
-<<<<<<< HEAD
-                Compartment findCompartment(int index) const;
-
-                Compartment &operator[](int index);
-                friend std::ostream& operator<<(std::ostream &os, const Shelf& shelf);
-=======
                 Compartment &operator[](size_t index);
                 friend std::ostream& operator<<(std::ostream& os, const Shelf& shelf);
->>>>>>> 86f4787eb85cbeb5450060fdf831e054133d8b24
 
         private:
                 Compartment compartments[15];
